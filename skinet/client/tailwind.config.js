c48740a671ed38abb7d35ad49fb1d4d60d6acaea
Shubham00117 <shubhamshinde5150@gmail.com>
2025-03-20 12:20:07 +0530

    End Of Section 8

--- skinet/client/tailwind.config.js
+++ skinet/client/tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js}"],
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
